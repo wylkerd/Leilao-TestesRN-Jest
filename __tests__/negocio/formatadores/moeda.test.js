9e48e38bc480506b2976745b7f9fdd7e28be30ee
Wylkerd Santos Silva <wylkerd.silva@modalgr.com.br>
2023-01-04 11:57:16 -0300

    chore: comentários sobre os principais métodos globais do Jest

--- __tests__/negocio/formatadores/moeda.test.js
+++ __tests__/negocio/formatadores/moeda.test.js
@@ -29,15 +29,37 @@ describe("negocio/formatadores/moeda", () => {
 })
 
 /**
- * Para checar se os valores nos testes são os esperados, usamos expects. Na biblioteca jest, existem várias formas de fazer isso, sendo as principais:
+ * Para checar se os valores nos testes são os esperados, usamos expects. Na biblioteca Jest, existem várias formas de fazer isso, sendo as principais:
 
-  toBe(): compara inteiros ou textos;
+    toBe(): compara inteiros ou textos;
 
-  toBeCloseTo(): compara pontos flutuantes obtidos através de operações matemáticas, pois, devido a arredondamentos, podem haver erros com toBe();
+    toBeCloseTo(): compara pontos flutuantes obtidos através de operações matemáticas, pois, devido a arredondamentos, podem haver erros com toBe();
 
-  toBeFalsy()/toBeTruthy(): compara valores falsos/verdadeiros em um contexto booleano. No caso de falsy, não apenas false será validado, mas valores como null, 0, '', 
-      undefined e NaN também. O restante dos valores é considerado truthy;
+    toBeFalsy()/toBeTruthy(): compara valores falsos/verdadeiros em um contexto booleano. No caso de falsy, não apenas false será validado, mas valores como null, 0, '', 
+        undefined e NaN também. O restante dos valores é considerado truthy;
 
-  toEqual(): compara objetos, verificando se as propriedades internas são iguais. Usar toBe() não retornará o mesmo resultado.
+    toEqual(): compara objetos, verificando se as propriedades internas são iguais. Usar toBe() não retornará o mesmo resultado.
 
 */
+
+/**
+ * Nesta aula, usamos as funções describe e it com frequência. Porém, elas não são as únicas funções globais que existem no Jest. Podemos usar algumas funções para controlar 
+ * quais métodos de    teste serão executados ou até executar funções antes/depois das funções de teste.
+
+  Veja abaixo as funções globais mais comuns:
+
+    describe('', () => {}): cria um contexto com uma descrição para todos os testes dentro da função;
+
+    test('', () => {}): cria um teste com uma descrição que deve ser correspondida ao que o teste pretende testar.
+
+    it('', () => {}): funciona exatamente igual ao test('', () => {}). É usado para começar a frase do teste, geralmente em inglês, 
+        onde a palavra it não precisa ser repetida. Exemplo: it('deve retornar verdadeiro') ou it('must return true');
+
+    afterAll(() => {}): executa a função após todos os testes do seu contexto (arquivo ou describe) terminarem sua execução;
+
+    beforeAll(() => {}): executa a função antes que todos os testes do seu contexto (arquivo ou describe) comecem sua execução;
+
+    afterEach(() => {}): executa a função várias vezes, sempre que um teste do seu contexto (arquivo ou describe) terminarem sua execução;
+    
+    beforeEach(() => {}): executa a função várias vezes, sempre antes que um teste do seu contexto (arquivo ou describe) começar sua execução.
+ */
